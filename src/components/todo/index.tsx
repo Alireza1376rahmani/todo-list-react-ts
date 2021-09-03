737a2af704ff501cc2f79e7c72c95fd5860ad033
alireza1376rahmani <alireza1376rahmani@gmail.com>
2021-09-03 11:12:31 +0430

    START phase 2

--- src/components/todo/index.tsx
+++ src/components/todo/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import AddTaskForm from "./AddTaskForm";
 import TaskList from "./TaskList";
-import "./style.css"
+import "./style.css";
 
 function TodoApp() {
 	return (
